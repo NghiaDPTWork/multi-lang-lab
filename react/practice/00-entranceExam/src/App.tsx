7571f0dd9b3795f0f76eafc01922da3f31d5b1f5
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-13 07:47:08 +0700

    feat: implement dynamic status and notifications based on click count

--- react/practice/00-entranceExam/src/App.tsx
+++ react/practice/00-entranceExam/src/App.tsx
@@ -5,6 +5,24 @@ function App() {
   const [count, setCount] = useState(0);
   const userName = "Nghia Duong";
 
+  let statusText = "Normal";
+  let statusColor = "green";
+
+  if (count > 5) {
+    statusText = "Too many!";
+    statusColor = "red";
+  } else if (count > 3) {
+    statusText = "Getting high...";
+    statusColor = "orange";
+  }
+
+  let messageText = "";
+  if (count === 1) {
+    messageText = "Good start!";
+  } else if (count === 10) {
+    messageText = "You reached 10!";
+  }
+
   return (
     <>
       <section id="center">
@@ -14,10 +32,24 @@ function App() {
         <button
           type="button"
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => setCount((prev) => prev + 1)}
         >
           Count is {count}
         </button>
+
+        <div style={{ marginTop: "1.5rem" }}>
+          <p style={{ fontWeight: "bold", margin: "0" }}>
+            Trạng thái: <span style={{ color: statusColor }}>{statusText}</span>
+          </p>
+        </div>
+
+        {messageText && (
+          <div
+            style={{ marginTop: "0.5rem", fontStyle: "italic", color: "#555" }}
+          >
+            Thông báo: {messageText}
+          </div>
+        )}
       </section>
 
       <div className="ticks"></div>
