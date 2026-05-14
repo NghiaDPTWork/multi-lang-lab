a0ce97aacfcee71282619fced4d149f17eebac1a
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-14 15:26:55 +0700

    feat(react): implement rendering a static list of to-dos

--- react/practice/01-react-fundation/src/App.tsx
+++ react/practice/01-react-fundation/src/App.tsx
@@ -1,7 +1,21 @@
 function App() {
+  // BƯỚC 1: Tạo một danh sách "cứng" (tĩnh) bằng mảng các chuỗi để tập hiển thị dữ liệu
+  const todos = [
+    "Học React căn bản",
+    "Làm bài tập JavaScript",
+    "Đi chợ mua đồ",
+  ];
+
   return (
     <div>
-      <h1>01-react-fundation</h1>
+      <h1>To-do List</h1>
+
+      {/* Hiển thị danh sách ra màn hình bằng hàm .map() */}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
