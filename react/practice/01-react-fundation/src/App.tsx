3440d7ca05a66d0ee70c5e454595f04d694cc06e
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-14 15:35:50 +0700

    feat(react): use useState to track and display real-time text input

--- react/practice/01-react-fundation/src/App.tsx
+++ react/practice/01-react-fundation/src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"; // 1. Nhập khẩu (import) useState để sử dụng vùng nhớ cho App
+
 function App() {
   const todos = [
     "Học React căn bản",
@@ -5,14 +7,25 @@ function App() {
     "Đi chợ mua đồ",
   ];
 
+  const [todo, setTodo] = useState("");
+
   return (
     <div>
       <h1>To-do List</h1>
 
       <div>
-        <input type="text" placeholder="Nhập công việc mới..." />
+        <input
+          type="text"
+          placeholder="Nhập công việc mới..."
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
         <button>Thêm</button>
       </div>
+
+      <p style={{ color: "gray" }}>
+        Nội dung bạn đang nhập: <b>{todo}</b>
+      </p>
 
       <ul>
         {todos.map((todo, index) => (
