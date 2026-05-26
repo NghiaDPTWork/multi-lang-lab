7c9044cba0b8041c66eb411056c8e092094109d3
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-26 11:02:43 +0700

    feat(profile): fetch mock user data and handle loading/error/empty states in ProfilePage

--- react/lessons/02-code-reference/src/pages/ProfilePage.tsx
+++ react/lessons/02-code-reference/src/pages/ProfilePage.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import {
+  EmptyState,
+  ErrorState,
+  LoadingState,
+} from "../components/ui/StatusStates";
+
 export default function ProfilePage() {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | any>(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/8")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Lỗi khi tải dữ liệu");
+        } else {
+          return response.json();
+        }
+      })
+      // .then((json) => console.log(json))
+      // .then((data) => setUser(data))
+      .catch(() => setError("Không tải được dữ liệu"))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <LoadingState />;
+  if (error) return <ErrorState message={error} />;
+  if (!user)
+    return <EmptyState message="Không tìm thấy thông tin người dùng" />;
+
   return (
     <div className="p-10 bg-white rounded-lg shadow-md">
       <h1 className="text-3xl font-bold mb-4">My Profile</h1>
-      <p className="text-gray-600">Chào mừng bạn đã trở lại!</p>
+      <p className="text-gray-600">Chào mừng bạn đã trở lại, {user?.name}!</p>
     </div>
   );
 }
