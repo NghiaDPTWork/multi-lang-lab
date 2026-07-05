57a5b5c677ec541141f7539b02d65020821c0f69
Tr.Nghia <t.nghia2112279@gmail.com>
2026-07-05 11:06:14 +0700

    chore(practice/11): update auth store configuration

--- 6-react/practice/11-miniProject/src/features/auth/store.ts
+++ 6-react/practice/11-miniProject/src/features/auth/store.ts
@@ -1,4 +1,20 @@
 import { create } from "zustand";
 import type { AuthAction, AuthState } from "./types";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
-export const useAuthStore = create<AuthState & AuthAction>();
+export const useAuthStore = create<AuthState & AuthAction>()(
+  devtools(
+    persist(
+      (set) => ({
+        accessToken: null,
+        role: null,
+        setAuth: ({ accessToken, role }) => set({ accessToken, role }),
+        clearAuth: () => set({ accessToken: null, role: null }),
+      }),
+      {
+        name: "auth-storage-practice",
+        storage: createJSONStorage(() => localStorage),
+      },
+    ),
+  ),
+);
