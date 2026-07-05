9c6e6b45628f5e880da39ec090ed4fc021727897
Tr.Nghia <t.nghia2112279@gmail.com>
2026-07-05 11:24:01 +0700

    feat(practice/11): implement api client interceptors and user auth services

--- 6-react/practice/11-miniProject/src/features/auth/service.ts
+++ 6-react/practice/11-miniProject/src/features/auth/service.ts
@@ -1,8 +1,16 @@
+import apiClient from "@/lib/axios";
 import type { AuthResponse, User, userLogin, userRegister } from "./types";
 
 export const UserService = {
-  async login(_credentials: userLogin): Promise<AuthResponse> {
-    // TODO: Implement login API call
-    throw new Error("Not implemented");
+  async login(credentials: userLogin): Promise<AuthResponse> {
+    return apiClient.post<AuthResponse>("/auth/login", credentials);
+  },
+
+  async register(credentials: userRegister): Promise<AuthResponse> {
+    return apiClient.post<AuthResponse>("/auth/resgister", credentials);
+  },
+
+  async getMe(): Promise<User> {
+    return apiClient.get<User>("auth/me");
   },
 };
