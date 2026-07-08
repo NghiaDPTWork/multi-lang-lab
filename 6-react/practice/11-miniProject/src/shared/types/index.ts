92ff3d6f547857bbfb9d1c6d57748b64246757c0
Tr.Nghia <t.nghia2112279@gmail.com>
2026-07-08 09:44:19 +0700

    chore: fix types and sync BaseService implementation for lessons and practice

--- 6-react/practice/11-miniProject/src/shared/types/index.ts
+++ 6-react/practice/11-miniProject/src/shared/types/index.ts
@@ -4,3 +4,18 @@ export interface SelectOption {
   id: string;
   name: string;
 }
+
+// Use for base Service
+export interface PaginationMeta {
+  totalItems: number;
+  totalPages: number;
+  itemsPerPage: number;
+  currentPage: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
+
+export interface PaginationResponse<T> {
+  data: T[];
+  meta: PaginationMeta;
+}
